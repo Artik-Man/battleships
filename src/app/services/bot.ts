ea95ec5015e257f091d17e2684749a280d24ab37
Artik <artik.alpha@gmail.com>
2019-03-16 15:34:46 +0300

    UPd: Bot smart shots

--- src/app/services/bot.ts
+++ src/app/services/bot.ts
@@ -9,7 +9,7 @@ Injectable();
  */
 export class Bot {
     private battlefield: BattleFieldCell[] = [];
-    private canShot = [];
+    private canShot: number[] = [];
 
     constructor() {
         console.log(this);
@@ -20,14 +20,14 @@ export class Bot {
      * Calls GameService.enemyFire method with random point
      * @param fire - GameService.enemyFire method
      */
-    public myTurn(fire: (point: Point) => boolean) {
-        const point = this.randomPoint();
+    public myTurn(fire: (point: Point) => boolean, point?: Point) {
+        point = point || this.randomPoint();
         const hit = fire(point);
         if (hit) {
             this.battlefield[point.y * BATTLEFIELD_SIZE + point.x].shipHere = true;
             this.battlefield[point.y * BATTLEFIELD_SIZE + point.x].shipDamaged = true;
             // TODO: need to understand where bot can take next shot
-            this.myTurn(fire);
+            this.myTurn(fire, this.smartShot(point));
         } else {
             this.battlefield[point.y * BATTLEFIELD_SIZE + point.x].miss = true;
         }
@@ -46,6 +46,33 @@ export class Bot {
                 this.battlefield.push(new BattleFieldCell(x, y));
             }
         }
+    }
+
+    /**
+     * Makes next shot near current shot
+     * @param point current shot position
+     * @returns point for shot or null
+     */
+    private smartShot(point: Point): Point {
+        const linearCoord = (p: Point) => p.y * BATTLEFIELD_SIZE + p.x;
+        const smartPoints: Point[] = [
+            { x: point.x - 1, y: point.y },
+            { x: point.x + 1, y: point.y },
+            { x: point.x, y: point.y - 1 },
+            { x: point.x, y: point.y + 1 }
+        ];
+        const points = smartPoints
+            .filter(p => {
+                return p.x >= 0 && p.y >= 0
+                    && p.x <= BATTLEFIELD_SIZE && p.y <= BATTLEFIELD_SIZE
+                    && this.canShot.includes(linearCoord(p));
+            })
+            .map(linearCoord);
+        if (!points.length) {
+            return null;
+        }
+        const index = Math.floor(Math.random() * smartPoints.length);
+        return smartPoints[index];
     }
 
     /**
